5c720baa9e03c400764e37a8f782d7c018ab18d0
Daniel Castillo Ruiz <danicaru96@gmail.com>
2021-01-27 19:41:06 +0100

    Estructua Operaciones

--- calculadora/src/calculadora/Operaciones.java
+++ calculadora/src/calculadora/Operaciones.java
@@ -1,14 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package calculadora;
 
-/**
- *
- * @author ALUMNO_MALAGA_ESTE
- */
+
 public class Operaciones {
+    double n1;
+    double n2;
+    double resultado;
+    char operacion;
     
+    public Operaciones(double n1, double n2, char operacion){
+        this.n1=n1;
+        this.n2=n2;
+        this.operacion=operacion;
+    }
+    
+    public void mostrarResultadoDoble(){
+        System.out.println(this.n1+" "+this.operacion+" "+this.n2+"  = "+this.resultado);
+    }
+    
+    public double getN1(){
+        return n1;
+    }
+    public void setN1(double n1){
+        this.n1=n1;
+    }
+     public double getN2() {
+        return n2;
+    }
+
+    public void setN2(double n2) {
+        this.n2 = n2;
+    }
+
+    public char getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(char operacion) {
+        this.operacion = operacion;
+    }
+
+    public double getRes() {
+        return resultado;
+    }
+
+    public void setRes(double res) {
+        this.resultado = resultado;
+    }
 }
