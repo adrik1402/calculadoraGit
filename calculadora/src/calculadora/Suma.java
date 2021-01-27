e57977795abfcc316ee580e827c6afcb07c79347
Daniel Castillo Ruiz <danicaru96@gmail.com>
2021-01-27 19:56:55 +0100

    Clase Suma

--- calculadora/src/calculadora/Suma.java
+++ calculadora/src/calculadora/Suma.java
@@ -1,13 +1,13 @@
 
 package calculadora;
 
-public class Suma extends Operaciones {
+public class Suma extends Estructura {
     
     private double suma;
     
     public Suma(double n1,double n2){
         super(n1,n2,'+');
         this.suma= n1 + n2;
-        this.setRes(this.suma);
+        this.setResultado(this.suma);
     }
 }
