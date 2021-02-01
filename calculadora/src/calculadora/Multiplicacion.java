6c2ec3177e73cc3634abd3dba3cef2d0d3792984
eldelgorrodepesca <jorge.ma36880@cesurformacion.com>
2021-02-01 20:20:02 +0100

    he arreglado la clase raizcuadrada y cambiado el signo del println de la clase multiplicacion, salia el signo de suma Amen.

--- calculadora/src/calculadora/Multiplicacion.java
+++ calculadora/src/calculadora/Multiplicacion.java
@@ -5,7 +5,7 @@ class Multiplicacion extends Estructura{
     
     private double multiplicacion;
     public Multiplicacion(double n1,double n2){
-        super(n1,n2,'+');
+        super(n1,n2,'x');
         this.multiplicacion=n1*n2;
         this.setResultado(this.multiplicacion);
     }
--- calculadora/src/calculadora/RaizCuadrada.java
+++ calculadora/src/calculadora/RaizCuadrada.java
@@ -1,11 +1,11 @@
+
 package calculadora;
 
 public class RaizCuadrada extends EstructuraSimple{
 	private double raiz;
 	
 	public RaizCuadrada(double n1) {
-		super(n1, "raiz");
-		n1=Math.sqrt(n1);
+		super(n1,"raiz");
 		this.raiz=Math.sqrt(n1);
 		this.setResultadoSimple(this.raiz);
 	}
